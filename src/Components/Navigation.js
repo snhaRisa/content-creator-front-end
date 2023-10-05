992ab326c6bf9610c6c19f72a55b12662f8f4e05
Rishav <risasnha1999@gmail.com>
2023-10-05 20:37:56 +0530

    Changes to AccountContainer

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -8,6 +8,7 @@ import Register from './Register';
 import Login from './Login';
 import AccountContainer from './AccountContainer';
 import CreatorForm from './CreatorForm';
+import SubscriptionPlanForm from './SubscriptionPlanForm';
 
 import PrivateRoute from './PrivateRoute';
 
@@ -45,6 +46,7 @@ const Navigation = (props)=>
             <Route path='/login' render={(props)=>{return <Login {...props} handleIsLog={handleIsLog}/>}} exact={true}/>
             <PrivateRoute path='/account' component={AccountContainer} exact={true}/>
             <PrivateRoute path='/change-to-creator' component={CreatorForm} exact={true}/>
+            <PrivateRoute path='/create-plans' component={SubscriptionPlanForm} exact={true}/>
         </>
     );
 };
