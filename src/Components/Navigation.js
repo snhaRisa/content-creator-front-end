619780c1bdf01398c3358fbe5d33761577ec323a
amaresh <javaliamaresh@gmail.com>
2023-10-18 16:56:48 +0530

    create post link

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -15,45 +15,46 @@ import CreateContent from './CreateContent'
 import SubscriptionPlan from './SubscriptionPlan';
 import Success from './Success';
 import Cancel from './Cancel';
-import PrivateRoute from './PrivateRoute';
-import NewContent from './CreateContent'
+import PrivateRoute from './PrivateRoute'
+// import NewContent from './CreateContent'
 
-const Navigation = (props)=>
-{
-    const {isLog, handleIsLog} = props; 
-    const dispatch = useDispatch(); 
+const Navigation = (props) => {
+    const { isLog, handleIsLog } = props
+    const dispatch = useDispatch()
 
-    return(
+    return (
         <div className='container md-5 pd-2'>
-            <ul className='nav' style={{backgroundColor:'white'}}>
-                <li className='nav-item'><Link to='/' className='nav-link' style={{color:'brown'}}>Home</Link></li>
+            <ul className='nav' style={{ backgroundColor: 'white' }}>
+                <li className='nav-item'><Link to='/' className='nav-link' style={{ color: 'brown' }}>Home</Link></li>
                 {
                     isLog ?
-                    <>
-                        <li><Link to='/account' className='nav-link' style={{color:'brown'}}>Account</Link></li>
-                        <li><Link to='/' style={{color:'white'}} onClick={()=>
-                        {
-                            localStorage.removeItem('token');
-                            handleIsLog();
-                            dispatch(removeUser());
-                            Swal.fire('Successfully Logged-Out!');
-                        }} className='nav-link'>Logout</Link></li>
-                    </>
-                    :
-                    <>
-                        <li><Link to='/register' className='nav-link' style={{color:'brown'}}>Register</Link></li>
-                        <li><Link to='/login' className='nav-link' style={{color:'brown'}}>Log-In</Link></li>
-                    </>
+                        <>
+                            <li><Link to='/post-content' className='nav-link' style={{ color: 'brown' }}>Create Content</Link></li>
+                            <li><Link to='/account' className='nav-link' style={{ color: 'brown' }}>Account</Link></li>
+                            <li><Link to='/' style={{ color: 'white' }} onClick={() => {
+                                localStorage.removeItem('token');
+                                handleIsLog();
+                                dispatch(removeUser());
+                                Swal.fire('Successfully Logged-Out!');
+                            }} className='nav-link'>Logout</Link></li>
+
+                        </>
+                        :
+                        <>
+                            <li><Link to='/register' className='nav-link' style={{ color: 'brown' }}>Register</Link></li>
+                            <li><Link to='/login' className='nav-link' style={{ color: 'brown' }}>Log-In</Link></li>
+                        </>
                 }
             </ul>
-            <Route path='/' component={Home} exact={true}/>
-            <Route path='/register' component={Register} exact={true}/>
-            <Route path='/login' render={(props)=>{return <Login {...props} handleIsLog={handleIsLog}/>}} exact={true}/>
-            <PrivateRoute path='/account' component={AccountContainer} exact={true}/>
-            <PrivateRoute path='/change-to-creator' component={CreatorForm} exact={true}/>
-            <PrivateRoute path='/create-plans' component={SubscriptionPlan} exact={true}/>
-            <PrivateRoute path='/success' component={Success} exact={true}/>
-            <PrivateRoute path='/cancel' component={Cancel} exact={true}/>
+            <Route path='/' component={Home} exact={true} />
+            <Route path='/register' component={Register} exact={true} />
+            <Route path='/login' render={(props) => { return <Login {...props} handleIsLog={handleIsLog} /> }} exact={true} />
+            <PrivateRoute path='/account' component={AccountContainer} exact={true} />
+            <PrivateRoute path='/change-to-creator' component={CreatorForm} exact={true} />
+            <PrivateRoute path='/create-plans' component={SubscriptionPlan} exact={true} />
+            <PrivateRoute path='/success' component={Success} exact={true} />
+            <PrivateRoute path='/cancel' component={Cancel} exact={true} />
+            <PrivateRoute path='/post-content' component={CreateContent} exact={true} />
         </div>
     );
 };
