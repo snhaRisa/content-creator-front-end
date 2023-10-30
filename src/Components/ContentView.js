7b7845498a144fffe2eeeb63757076997ed005af
Rishav <risasnha1999@gmail.com>
2023-10-30 22:22:10 +0530

    Video Player Work

--- src/Components/ContentView.js
+++ src/Components/ContentView.js
@@ -296,6 +296,8 @@ const ContentView = (props)=>
         }
     };
 
+    console.log(singleContent);
+    const link = singleContent.fileType; 
     return(
         <div className='container'>
             {
@@ -307,9 +309,10 @@ const ContentView = (props)=>
                             <img className="card-img-top" src={singleContent.fileType} alt={singleContent.title} />
                         ) 
                         : 
+                        singleContent.type === 'video' && 
                         (
                             <video className='card-img-top' controls width="100%" style={{ height: 'auto' }}>
-                                <source src={singleContent.fileType} />
+                                <source src={singleContent.fileType} type='video/mp4'/>
                             </video>
                         )}
                         <p className='card-body mt-3'>
